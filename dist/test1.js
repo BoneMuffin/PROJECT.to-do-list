3b029735359a7159329c8b387e9affb3b0fc5ccc
V <valen.bagaglia@gmail.com>
2023-07-31 21:44:00 +0200

    fix the damn circles status'

--- dist/test1.js
+++ dist/test1.js
@@ -1,47 +1,18 @@
 let doc = document;
+let statuses = ['undefined', 'started', 'halfway', 'done', 'canceled'];
 
-function getNextStatus(currentStatus) {
-  let statuses = ['undefined', 'started', 'halfway', 'done', 'canceled'];
-  let currentIndex = statuses.indexOf(currentStatus);
-  let nextIndex = (currentIndex + 1) % statuses.length;
-  return statuses[nextIndex];
-}
-
-function createNote() {
-  let note = document.createElement('div');
-  note.classList.add('note');
-
-  let todo = document.createElement('div');
-  todo.classList.add('todo', 'undefined');
-
-  let text = document.createElement('div');
-  text.contentEditable = true;
-  text.classList.add('text');
-  text.textContent = 'Enter your note here';
-
-  note.appendChild(todo);
-  note.appendChild(text);
-
-  return note;
-}
-
-doc.addEventListener('DOMContentLoaded', () => {
-  let notesContainer = doc.querySelector('.notes');
-
-  // Create initial notes
-  for (let i = 0; i < 5; i++) {
-    let note = createNote();
-    notesContainer.appendChild(note);
-  }
-
-  // Event delegation for circle clicks
-  notesContainer.addEventListener('click', (e) => {
-    if (e.target.classList.contains('todo')) {
-      let currentStatus = e.target.classList.contains('todo') ? 'undefined' : e.target.classList[1];
-      let nextStatus = getNextStatus(currentStatus);
-
-      e.target.classList.remove(currentStatus);
-      e.target.classList.add(nextStatus);
+doc.querySelectorAll('todo').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let currentStatus = '';
+    for (let status of statuses) {
+      if (e.target.classList.contains(status)) {
+        currentStatus = status;
+        break;
+      }
     }
+
+    let index = statuses.indexOf(currentStatus);
+    let nextStatus = statuses[index + 1 >= statuses.length ? 0 : index + 1];
+    e.target.classList.replace(currentStatus, nextStatus);
   });
 });
