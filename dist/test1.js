b5050db1b65048008a7a822c6d488d805ec94513
V <valen.bagaglia@gmail.com>
2023-07-31 20:45:24 +0200

    garbage

--- dist/test1.js
+++ dist/test1.js
@@ -1,16 +1,47 @@
 let doc = document;
-let statuses = ['o', 'started', 'halfway', 'done', 'canceled'];
 
-doc.querySelectorAll('div.todo').forEach((el) => {
-  el.addEventListener('click', (e) => {
-    statuses.every((status, index) => {
-      if (e.target.classList.contains(status)) {
-        e.target.classList.replace(status, statuses[index + 1 > statuses.length - 1 ? 0 : index + 1]);
-        return false;
-      } else if (!e.target.classList.length || e.target.classList.contains('o')) {
-        e.target.classList.add(statuses[1]);
-        return false;
-      } else return true;
-    });
+function getNextStatus(currentStatus) {
+  let statuses = ['undefined', 'started', 'halfway', 'done', 'canceled'];
+  let currentIndex = statuses.indexOf(currentStatus);
+  let nextIndex = (currentIndex + 1) % statuses.length;
+  return statuses[nextIndex];
+}
+
+function createNote() {
+  let note = document.createElement('div');
+  note.classList.add('note');
+
+  let todo = document.createElement('div');
+  todo.classList.add('todo', 'undefined');
+
+  let text = document.createElement('div');
+  text.contentEditable = true;
+  text.classList.add('text');
+  text.textContent = 'Enter your note here';
+
+  note.appendChild(todo);
+  note.appendChild(text);
+
+  return note;
+}
+
+doc.addEventListener('DOMContentLoaded', () => {
+  let notesContainer = doc.querySelector('.notes');
+
+  // Create initial notes
+  for (let i = 0; i < 5; i++) {
+    let note = createNote();
+    notesContainer.appendChild(note);
+  }
+
+  // Event delegation for circle clicks
+  notesContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains('todo')) {
+      let currentStatus = e.target.classList.contains('todo') ? 'undefined' : e.target.classList[1];
+      let nextStatus = getNextStatus(currentStatus);
+
+      e.target.classList.remove(currentStatus);
+      e.target.classList.add(nextStatus);
+    }
   });
 });
